2bad15cf96918caabad8e1a233949e8067f1205f
Raymond Kroon <raymond@k3n.nl>
2019-05-14 17:14:12 +0200

    show pod ip with custom chart

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const color = process.env.APP_COLOR || 'blue';
+const podIP = process.env.POD_IP || 'unknown';
 
 const app = express();
 const port = 5000;
@@ -9,7 +10,7 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/', function (_, res) {
-    res.render('index', { title: `${color} site`, message: `Welcome to the ${color} site!`, color: `${color}`})
+    res.render('index', { title: `${color} site`, message: `Welcome to the ${color} site, hosted on pod with ip ${podIP}`, color: `${color}`})
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
