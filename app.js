1f03b16bdca40b284bf62d12aab2b829bbc4df8b
Raymond Kroon <raymond@k3n.nl>
2019-05-14 19:20:54 +0200

    site with env

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 
 const color = process.env.APP_COLOR || 'blue';
-const podIP = process.env.POD_IP || 'unknown';
+const podIP = process.env.POD_IP || '<unset>';
+const env = process.env.ENV || 'DEV';
 
 const app = express();
 const port = 5000;
+
+let hitCounter = 0;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/', function (_, res) {
-    res.render('index', { title: `${color} site`, message: `Welcome to the ${color} site, hosted on pod with ip ${podIP}`, color: `${color}`})
+    hitCounter++;
+    res.render('index', { title: `${color} site`, ip: podIP, counter: hitCounter, color: color, env: env})
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
