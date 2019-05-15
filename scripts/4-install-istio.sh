088d6a9b8b523c6fab660a91e4f1afda43808f79
Raymond Kroon <raymond@k3n.nl>
2019-05-15 22:22:44 +0200

    kiali (overschrijft gke istio)

--- scripts/4-install-istio.sh
+++ scripts/4-install-istio.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+kubectl create clusterrolebinding cluster-admin-binding \
+    --clusterrole=cluster-admin \
+    --user=$(gcloud config get-value core/account)
+
 cluster_name=${1:-blue-green-cluster}
 
 gcloud beta container clusters update ${cluster_name} \
---
+++ scripts/6-install-kiali.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+KIALI_USERNAME=$(read -p 'Kiali Username: ' uval && echo -n $uval | base64)
+KIALI_PASSPHRASE=$(read -sp 'Kiali Passphrase: ' pval && echo -n $pval | base64)
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: kiali
+  namespace: istio-system
+  labels:
+    app: kiali
+type: Opaque
+data:
+  username: $KIALI_USERNAME
+  passphrase: $KIALI_PASSPHRASE
+EOF
+
+kubectl apply -f istio-kiali.yaml
